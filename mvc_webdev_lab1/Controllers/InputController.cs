702de4875482f3a39fcdd4cb4f1d2e1879b3f4e7
Danil D <kreptoID41@yandex.ru>
2024-06-14 21:27:53 +0700

    Add project files.

---
+++ mvc_webdev_lab1/Controllers/InputController.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using mvc_webdev_lab1.Models;
+
+namespace mvc_webdev_lab1.Controllers
+{
+    public class InputController : Controller
+    {
+        public IActionResult Task1()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Task1(InputViewModel model)
+        {
+            int spaceCount = model.InputSentence.Count(c => c == ' ');
+            ViewData["SpaceCount"] = spaceCount;
+            return View();
+        }
+
+        public IActionResult Task2()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Task2(InputViewModel model)
+        {
+            string replacedSentence = model.InputSentence.Replace(' ', '_');
+            ViewData["ReplacedSentence"] = replacedSentence;
+            return View();
+        }
+    }
+}
---
+++ mvc_webdev_lab1/Models/InputViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace mvc_webdev_lab1.Models
+{
+    public class InputViewModel
+    {
+        public string InputSentence { get; set; }
+    }
+}
